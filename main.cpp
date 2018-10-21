e48b887e0738fd02666ec2f71db086b381ed3668
Ankur Solanky <solankya142127@student.vvc.edu>
2018-10-20 23:53:26 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,51 @@
 /**
 
-Description:
-Filename:
-Author:
-Date:
+Description: This program will prompt a user to enter in a string of letters and will return the number of m's in the entered string.  
+Filename: main.cpp
+Author: Ankur Solanky  
+Date: Thu Oct 18 18:22:44 PDT 2018
 
 */
 
+#include<iostream>
+#include<string>
+
 using namespace std;
 
+string get_string(string prompt)
+{
+        string s;
+        cout << prompt;
+        getline(cin, s);
+
+        return s;
+}
+
+int char_count(string question, char m)
+{   
+    int count = 0;
+    for (int i = 0; i < question.size(); i++)
+    {
+        char letter = tolower(question[i]); // This makes an uppercase letter into lowercase. 
+        if(letter == 'm')
+        {
+            count++;
+        }
+    } 
+    return count;
+}    
 
 int main()
 {
-   return 0;
+    string task = get_string("Please enter a string letters: ");
+    cout << "Please enter a letter: ";
+    char m;
+    cin >> m;
+    int count = char_count(task,m);
+    cout << "There are " << count << " letter m's in the string: " << task << endl; 
+    
+    return 0;  
 }
+
+
+
